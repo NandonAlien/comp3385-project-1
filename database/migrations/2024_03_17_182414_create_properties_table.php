10a954c3e9e3842693b30646ff143fb6fd06af77
Nandon <nandonarts@gmail.com>
2024-03-17 14:53:35 -0400

    Adding towards form and Creating a Model and Migration

---
+++ database/migrations/2024_03_17_182414_create_properties_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('property-title');
+            $table->text('description');
+            $table->integer('room-amt');
+            $table->integer('bathroom-amt');
+            $table->float('price');
+            $table->string('property-type');
+            $table->string('location');
+            $table->string('company_logo');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
--- resources/views/property-form.blade.php
+++ resources/views/property-form.blade.php
@@ -1,5 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-
+<h1>Create Client</h1>
+<form action = {{ url('/client')}} method="post" enctype="multipart/form-data" >
+    @csrf
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <div>
+        <label for='property-title'>Property Title</label><br>
+        <input type="text" id="property-title" name="property-title">
+    </div>
+    <div>
+        <label for='description'>Description</label><br>
+        <textarea id="description" name="description"></textarea>
+    </div>
+    <div>
+        <label for='room-amt'>No. of Rooms</label><br>
+        <input type="number" id="room-amt" name="room-amt">
+    </div>
+    <div>
+        <label for='bathroom-amt'>No. of Bathrooms</label><br>
+        <input type="number" id="bathroom-amt" name="bathroom-amt">
+    </div>
+    <div>
+        <label for='price'>Price</label><br>
+        <input type="number" id="price" name="price">
+    </div>
+    <div>
+        <label for='property-type'>Property Type</label><br>
+        <input type="text" id="property-type" name="property-type">
+    </div>
+    <div>
+        <label for='location'>Location</label><br>
+        <input type="text" id="location" name="location+">
+    </div>
+    <div>
+        <label for='photo'>Photo</label><br>
+        <input type="file" id="photo" name="photo" accept ="image/*">
+    </div>
+    <button type="submit">Add Property</button>
+</form>
 @endsection
