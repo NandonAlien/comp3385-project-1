7811cf2af7115436e8fb7954f2a2d85a2ce81e95
Nandon <nandonarts@gmail.com>
2024-03-18 01:36:33 -0400

    Final changes to the project

--- resources/views/properties.blade.php
+++ resources/views/properties.blade.php
@@ -7,15 +7,15 @@
 {{ session()->get('success') }}
 </div>
 @endif
-<div class="flex-row">
+<div class="flex-row flex-wrap">
 @foreach($properties as $property)
     <div class="card w-25">
-        <img class="card-img-top"src= "{{asset('storage/'.$property->photoName)}}" alt = '{{$property->title}}'>
+        <img class="card-img-top"src= "{{asset('storage/images/'.$property->photoName)}}" alt = '{{$property->title}}'width="400' height="200">
         <div class="card-body">
         <h1 class="card-title">{{$property->roomAmt}} Bedroom {{$property->title}} </h1>
         <div class="d-flex">
-        <p class="card-text mt-2 px-2">{{$property->location}}</p>
-        <p class="card-text mt-2 px-2">$ {{$property->price}}</p>
+        <p class="card-text bg-secondary text-white rounded">{{$property->location}}</p>
+        <p class="card-text bg-primary text-white rounded">${{$property->price}}</p>
         </div>
         </div>
         <a class="bg-success text-white card-footer" href ='/properties/{{$property->id}}'>View Property</a>
--- resources/views/property.blade.php
+++ resources/views/property.blade.php
@@ -1,15 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-        <img src= "{{asset('storage/'.$product->photoName)}}" alt = '{{$product->title}}'>
+<div class = "card w-30 flex-row">
+        <img src= "{{asset('storage/images/'.$product->photoName)}}" alt = '{{$product->title}} width="600' height="400">
+
+<div class="card-body">
         <h2>{{$product->title}}</h2>
-        <p>{{$product->price}}</p>
+        <div class="row">
+        <p class="w-10 col-*-* bg-primary text-white rounded">${{$product->price}}</p>
+        <p class="w-10 col-*-* bg-secondary text-white rounded">{{$product->type}}</p>
+        </div>
         <p>{{$product->description}}<p>
-        <p>{{$product->roomAmt}} Bedrooms</p>
-        <p>{{$product->bathroomAmt}} Bathrooms</p>
+        <div class="row">
+        <p class="col-*-*">{{$product->roomAmt}} Bedrooms</p>
+        <p class="col-*-*">{{$product->bathroomAmt}} Bathrooms</p>
+        </div>
         <p>{{$product->location}}</p>
-        <button>Email</button>
+        <button class="btn btn-primary">Email Realtor</button>
+</div>
+<div class="card-footer">
         
+</div>
 </div>
 @endsection
